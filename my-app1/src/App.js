8056e0a293734adaca219d5b63f3c46a02de69cf
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-09-25 02:22:33 +1000

    implemented class components

---
+++ my-app1/src/App.js
@@ -0,0 +1,23 @@
+import {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi Vicky
+          </p>
+          <button>
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
