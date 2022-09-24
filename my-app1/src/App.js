41ab0742408b1343ef14f4a63a0957ec7131de17
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-09-25 02:30:26 +1000

    added state components

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -3,13 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: 'Shalu'
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Vicky
+            Hi {this.state.name}
           </p>
           <button>
             Change name
